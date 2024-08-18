43a1aaacf24da50b0deccf0ea0e86e0e7030ae7b
patelhejal <hejal.itjobs@gmail.com>
2024-08-18 10:18:53 -0400

    Spring mvc first project is committed

---
+++ src/main/java/edu/webmvc/com/SpringWebMvc01Application.java
@@ -0,0 +1,13 @@
+package edu.webmvc.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/webmvc/com/SpringWebMvc01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.webmvc.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvc01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
